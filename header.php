2398650dda97f449616b194fbdc73d515d919b24
funny-vince <funny-vince@hotmail.com>
2015-02-24 15:06:22 -0500

    commit quelques images + page magasin

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Home | E-Shop</title>
+    <title>Accueil | Le gîte du cerf-volant</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/prettyPhoto.css" rel="stylesheet">
@@ -25,7 +25,30 @@
 </head><!--/head-->
 
 <body>
-	<header id="header"><!--header-->	
+	<header id="header"><!--header-->
+
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="index.php">Le gîte du cerf-volant</a>
+						</div>
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href=""><i class="fa fa-user"></i> Compte</a></li>
+								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Panier</a></li>
+								<li><a href="login.html"><i class="fa fa-pencil"></i> Inscription</a></li>
+								<li><a href="login.html"><i class="fa fa-lock"></i> Connexion</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
 		<div class="header-bottom"><!--header-bottom-->
 			<div class="container">
 				<div class="row">
@@ -41,9 +64,9 @@
 						<div class="mainmenu pull-left">
 							<ul class="nav navbar-nav collapse navbar-collapse">
 								<li><a href="index.php" class="active">Accueil</a></li>
-								<li class="dropdown"><a href="#">Produits<i class="fa fa-angle-down"></i></a>
+								<li class="dropdown"><a href="shop.php">Produits<i class="fa fa-angle-down"></i></a>
 		                            			<ul role="menu" class="sub-menu">
-		                                			<li><a href="shop.html">Liste des Produits</a></li>
+		                                			<li><a href="shop.php">Liste des Produits</a></li>
 									<li><a href="product-details.html">Promotions</a></li>  
 									<li><a href="cart.html">Panier</a></li>  
 		                            			</ul>
@@ -57,7 +80,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-4">
+					<div class="col-sm-3 pull-right">
 						<div class="search_box">
 							<form method="post" action="search.php">
 								<input type="text" placeholder="Recherche" class="col-sm-10"/>
@@ -65,14 +88,6 @@
 									<span class="glyphicon glyphicon-search"></span>
 								</button>
 							</form>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="mainmenu pull-right">
-							<ul class="nav navbar-nav collapse navbar-collapse">
-								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Panier</a></li>
-								<li><a href="login.html"><i class="fa fa-lock"></i> Connexion</a></li>	
-							</ul>
 						</div>
 					</div>
 				</div>
