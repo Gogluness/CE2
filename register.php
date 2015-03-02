5f40303fe06b76093faf1df728442e15ee03717a
Vincent Bernier <gogluness@gmail.com>
2015-03-02 05:36:16 -0800

    Register done

--- register.php
+++ register.php
@@ -2,26 +2,26 @@
 <?php
 $prenom = $_POST["prenom"];
 $nom = $_POST["nom"];
-$username = $_POST["username"];
+$email = $_POST["username"];
 $password = $_POST["password"];
 $confirmPassword = $_POST["confirmPassword"];
 
-$servername = "205.236.12.52";
-$username = "equipe3h15";
-$password = "ebola-cerf";
-$dbname = "equipe3h15";
+$servername = "localhost";
+$username = "root";
+$password = "admin123";
+$dbname = "CE2";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "INSERT INTO users (Prenom, Nom, Username, Password)
+    $sql = "INSERT INTO Users (Prenom, Nom, Username, Password)
     VALUES ('$prenom', '$nom', '$username', '$password')";
 
     // use exec() because no results are returned
     $conn->exec($sql);
-    echo "Ajout de " . $nom . " " . $prenom . " username " . $username;
+    echo "Ajout de " . $nom . " " . $prenom . " username " . $email;
     }
 
     //email de confirmation d'inscription
