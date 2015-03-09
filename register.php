44c065eaecdb728d54ed14fab1941106425fc411
gogluness <gogluness@gmail.com>
2015-03-09 13:41:14 -0700

    Register 100%

--- register.php
+++ register.php
@@ -1,6 +1,44 @@
 <?php
     include "header.php" // Includes Login Script
 ?>
+<?php
+if (isset($_POST['submit']))
+    {
+    $error=''; // Variable To Store Error Message
+
+            $nom = $_POST["nom"];
+            $prenom = $_POST["prenom"];
+            $loginuser = $_POST["email"];
+            $loginpassword = $_POST["password"];
+            $confirmpassword = $_POST["confirmPassword"];
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "admin123";
+            $dbname = "CE2";
+
+            // Create connection
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            // Check connection
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            $sql = "INSERT INTO Users (Prenom, Nom, Username, Password) VALUES ('$prenom', '$nom', '$loginuser', '$loginpassword')";
+
+            if (mysqli_query($conn, $sql)) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+
+            mysqli_close($conn);
+
+    echo($error);
+	}
+	else
+	{
+		?>
 <section class="body-section">
 <div class="main">
 <div class="inner-main">
@@ -24,7 +62,7 @@
 		<div class="clear"> </div>	
       </div>
       <h2>S'enregistrer</h2>
-		<form action="registerbackend.php" method="post">
+		<form action="register.php" method="post">
 		   <div class="lable">
 		        <div class="col_1_of_2 span_1_of_2">	
 		        	<input type="text" class="text" value="Prenom" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Prenom';}" id="active" name="prenom">
@@ -43,7 +81,7 @@
 		   <h3>En vous enregistrant, vous acceptez nos <span class="term"><a href="#">Termes & Conditions</a></span></h3>
 		   </div>
 		   <div class="submit">
-			  <input type="submit" value="Enregistrer" >
+			  <input type="submit" name="submit" value="Enregistrer" >
 		   </div>
 		   <div class="clear"> </div>
 		</form>
@@ -57,5 +95,9 @@
 		</div>
 				<!-----//end-copyright---->
 </section>
+		<?php
+	}
+?>
 
-<?php include "footer.php"?>
+<?php include "footer.php" ?>
+
--- registerbackend.php
+++ registerbackend.php
@@ -2,6 +2,11 @@
     echo(include "header.php"); // Includes Login Script
     $error=''; // Variable To Store Error Message
 
+            $nom = $_POST["nom"];
+            $prenom = $_POST["prenom"];
+            $loginuser = $_POST["email"];
+            $loginpassword = $_POST["password"];
+            $confirmpassword = $_POST["confirmPassword"];
 
             $servername = "localhost";
             $username = "root";
@@ -15,7 +20,7 @@
                 die("Connection failed: " . mysqli_connect_error());
             }
 
-            $sql = "INSERT INTO Users (Prenom, Nom, Username, Password) VALUES ('monprenom2', 'monnom', 'monemail', 'password')";
+            $sql = "INSERT INTO Users (Prenom, Nom, Username, Password) VALUES ('$prenom', '$nom', '$loginuser', '$loginpassword')";
 
             if (mysqli_query($conn, $sql)) {
                 echo "New record created successfully";
@@ -26,4 +31,5 @@
             mysqli_close($conn);
 
     echo($error);
+    echo(include "footer.php");
 ?>
