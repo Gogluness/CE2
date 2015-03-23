b22b652316c350fbca41b948f8e32830fdb8c993
gogluness <gogluness@gmail.com>
2015-03-23 05:53:48 -0700

    fichiers inutiles

--- PDO/bd_connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=CE2;charset=utf8', 'root', 'admin123');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-?>
